2e4fa27fa8ffcf5cb788ae61b89b283c7f4ce3a8
Adedayo Ogunleye <adedayoogunleye2@gmail.com>
2023-11-28 14:58:23 -0800

    My task

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,25 +1,23 @@
-# File:   7-puppet_install_nginx_web_server.pp
-# Author: Adedayo Ogunleye
-# email:  adedayoogunleye2@gmail.com
-
-# Using Puppet| Install Nginx server, setup and configuration
+# This manifest will install nginx on a server and configure the 301-redirect
 
 package { 'nginx':
-  ensure => 'installed'
-}
+  ensure => installed,
+  }
 
-file { '/var/www/html/index.html':
-  content => 'Hello World',
-}
-
-file_line { 'redirection-301':
+file_line { 'configure 301-redirect page':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
   line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-}
+  }
 
+# add the line 'Hello World!' when nginx is queried using GET request
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+  }
+
+# restart nginx service afer config
 service { 'nginx':
   ensure  => running,
   require => Package['nginx'],
-}
+  }
